e7c95b9c7fb8fb5cbe128469929807e3e76b18fa
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2025-11-17 09:53:44 -0300

    Fix source command for biblioteca.sh

--- main.sh
+++ main.sh
@@ -14,7 +14,7 @@ BIBLIOTECA="${HOME}/repos/ngs-scripts/biblioteca.sh"
 
 if [[ -f "$BIBLIOTECA" ]]; then
 	echo "Carregando a biblioteca..."
-	source ".${BIBLIOTECA}"
+	source "$BIBLIOTECA"
 	# Testando a função qc
 	qc
 else
