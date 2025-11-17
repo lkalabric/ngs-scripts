b3eea5bf52b345c57cd36778d76402aa767e5b80
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2025-11-17 09:44:35 -0300

    Fix variable reference and error message in main.sh

--- main.sh
+++ main.sh
@@ -10,13 +10,13 @@
 # Requirements: fastqc
 
 # Funções disponíveis para execução do pipeline
-BIBLIOTECA="{HOME}/repos/ngs-scripts/biblioteca.sh"
+BIBLIOTECA="${HOME}/repos/ngs-scripts/biblioteca.sh"
 
 if [[ -f "$BIBLIOTECA" ]]; then
 	echo "Carregando a biblioteca..."
 	source $BIBLIOTECA
 else
-	echo "Biblioteca $BIBLIOTECA não disponível. Verifique com o desenvolvedor do seu pipeline!"
+	echo "Biblioteca não disponível. Verifique com o desenvolvedor do seu pipeline!"
 	exit
 fi
 
