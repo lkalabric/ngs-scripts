d09a414968aae4b9f81c7ec8999ad87c3c84b98d
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2025-11-17 09:06:14 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -27,32 +27,65 @@ fi
 INPUT_DIR=$RAWDIR
 
 # Cria o diretório de resultados, caso não exista
-OUTPUT_DIR="${HOME}/results/${LIBNAME}"
-if [ ! -d "$OUTPUT_DIR" ]; then
-    # O '!' inverte a condição (NÃO é um diretório)
-    echo "Diretório '$OUTPUT_DIR' não encontrado. Criando..."
-    mkdir -p "$OUTPUT_DIR"
+echo "Preparando pastas para (re-)análise dos dados..."
+OUTPUT_DIR="${HOME}/results/${LIBNAME}/wf${WF}"
+# Cria a pasta de resultados
+if [[ ! -d "${OUTPUT_DIR}" ]]; then
+	mkdir -vp ${OUTPUT_DIR}
 else
-    echo "Diretório '$OUTPUT_DIR' já existe. Ignorando a criação."
+	read -p "Re-analisar os dados [S-apagar e re-analisa os dados / N-continuar as análises de onde pararam]? " -n 1 -r
+	if [[ $REPLY =~ ^[Ss]$ ]]; then
+	  # Reseta a pasta de resultados do worflow
+		echo -e "\nApagando as pastas e reiniciando as análises..."
+		[[ ! -d "${OUTPUT_DIR}" ]] || mkdir -vp ${OUTPUT_DIR} && rm -r "${OUTPUT_DIR}"; mkdir -vp "${OUTPUT_DIR}"
+	fi
 fi
-
 # Parada para degub do código
 exit
 
+# Criar um input_validation.sh a partir do código abaixo
 # Validação dos dados
 # Lê o nome dos arquivos de entreda. O nome curto será o próprio nome da library
 # Renomear os arquivos R1 e R2 para conter o prefixo LIBNAME_ (ex. Bper42_xxxx)
-INDEX=0
-for FILE in $(find ${IODIR} -mindepth 1 -type f -name *.fastq.gz -exec basename {} \; | sort); do
-	FULLNAME[$INDEX]=${FILE}
-	((INDEX++))
-done
-SAMPLENAME=$(echo $FULLNAME[0] | cut -d "E" -f 1)
-LIBSUFIX=$(echo $LIBNAME | cut -d "_" -f 2)
-if [[ $SAMPLENAME -ne $LIBSUFIX ]]; then
-	echo "Você copiou os dados errados para a pasta $LIBNAME!"
-	exit 3
-fi
+#INDEX=0
+#for FILE in $(find ${IODIR} -mindepth 1 -type f -name *.fastq.gz -exec basename {} \; | sort); do
+#	FULLNAME[$INDEX]=${FILE}
+#	((INDEX++))
+#done
+#SAMPLENAME=$(echo $FULLNAME[0] | cut -d "E" -f 1)
+#LIBSUFIX=$(echo $LIBNAME | cut -d "_" -f 2)
+#if [[ $SAMPLENAME -ne $LIBSUFIX ]]; then
+#    echo "Você copiou os dados errados para a pasta $LIBNAME!"
+#    exit 3
+#fi
+
+
+FASTQCDIR="${RESULTSDIR}/FASTQC"
+TEMPDIR="${RESULTSDIR}/TEMP"
+TRIMMOMATICDIR="${RESULTSDIR}/TRIMMOMATIC"
+MUSKETDIR="${RESULTSDIR}/MUSKET"
+FLASHDIR="${RESULTSDIR}/FLASH"
+KHMERDIR="${RESULTSDIR}/KHMER"
+SPADESDIR="${RESULTSDIR}/SPADES"
+SPADES2DIR="${RESULTSDIR}/SPADES2"
+FLAG=0
+
+# Parâmetro de otimização das análises
+KMER=21 # Defaut MAX_KMER_SIZE=28. Se necessário, alterar o Makefile e recompilar
+THREADS="$(lscpu | grep 'CPU(s):' | awk '{print $2}' | sed -n '1p')"
+
+# Quality control report
+# Foi utilizado para avaliar o sequenciamento e extrair alguns parâmtros para o Trimmomatic
+# Link: https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
+function qc_bper () {
+	if [[ ! -d $FASTQCDIR ]]; then
+		mkdir -vp $FASTQCDIR
+		echo -e "Executando fastqc em ${IODIR}...\n"
+		fastqc --noextract --nogroup -o ${FASTQCDIR} ${IODIR}/*.fastq.gz
+	else
+		echo "Dados analisados previamente..."
+	fi
+}
 
 # Configuração das pastas de saída
 echo "Preparando pastas para (re-)análise dos dados..."
