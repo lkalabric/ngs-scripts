07d9116a6ea1f6a73593526a511610524392ff50
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2025-11-18 10:36:47 -0300

    Update biblioteca.sh

--- biblioteca.sh
+++ biblioteca.sh
@@ -226,7 +226,7 @@ function install_conda_packages_if_missing () {
 	        echo "   ❌ Ambiente '${ENV_NAME}' não encontrado."
 	        # Ambiente não existe: Usa 'create' para criar e instalar o pacote
 	        echo "   -> Criando novo ambiente e instalando '${PACKAGE_NAME}'..."
-	        INSTALLATION_COMMAND="conda create -n \"${ENV_NAME}\" \"${PACKAGE_NAME}\" -y"
+	        INSTALLATION_COMMAND="conda create -n \"${ENV_NAME}\ -y"; conda actuvate \"${ENV_NAME}\; conda install -c bioconda -c conda-forge -c defaults -n \"${ENV_NAME}\" \"${PACKAGE_NAME}\" -y"
 	    fi
 		
 	    # 3.3 Execução do comando
