2a4ad3904b7d181b741961ced9390202919216ab
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2025-12-01 12:49:24 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -107,7 +107,7 @@ WORKFLOWLIST=(
 	'config'
 	'magma'
 	'fastqc'
-	'setup_directories fastqc trim'
+	'setup_directories fastqc'
 	'spades_bper'
 	'trim_bper spades_bper'
 	'trim_bper musket_bper spades_bper'
