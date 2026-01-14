38ad57a27f0b613deb8ece90970fd48c7c59c0eb
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2026-01-14 16:23:58 -0300

    Fix variable usage in biblioteca.sh

--- biblioteca.sh
+++ biblioteca.sh
@@ -437,10 +437,10 @@ function fastqc () {
 	for base_name in $(find "$INPUT_DIR/." -maxdepth 1 -mindepth 1 -type d -exec basename {} \; | sort); do
 		FASTQC_DIR="$OUTPUT_DIR/$base_name/fastqc"
 		if [[ -n "$base_name" && ! -d "$FASTQC_DIR" ]]; then
-			echo "Criando a pasta de saída nos dados ${base_name}..."
+			echo "Criando a pasta de saída nos dados $base_name..."
 			mkdir -vp $FASTQC_DIR
-			echo -e "Executando o fastqc nos dados disponíveis em ${base_name}...\n"
-			fastqc --noextract --nogroup -o ${FASTQC_DIR} ${INPUT_DIR}/${base_name}/*
+			echo -e "Executando o fastqc nos dados disponíveis em $base_name...\n"
+			fastqc --noextract --nogroup -o $FASTQC_DIR "$INPUT_DIR/$base_name/*"
 		else
 			echo "Dados analisados previamente..."
 		fi
