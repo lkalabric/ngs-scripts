1272f486c8b3a0118ecc20c27e33dcddf4e759e1
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2026-01-14 16:17:40 -0300

    Refactor sample processing in biblioteca.sh

--- biblioteca.sh
+++ biblioteca.sh
@@ -432,19 +432,17 @@ function fastqc () {
 	
 	# Parâmetros padrões ou personalizados pelo usuário
 		source "${HOME}/repos/ngs-scripts/param/fastqc.param"
-	exit 1
-
+	
 	# Análise propriamente dita
-	for SAMPLE in $(find "$INPUT_DIR" -maxdepth 1 -mindepth 1 -type d -exec basename {} \; | sort); do
-		base_name=$(basename "$SAMPLE")
+	for base_name in $(find "$INPUT_DIR" -maxdepth 1 -mindepth 1 -type d -exec basename {} \; | sort); do
 		FASTQC_DIR="$OUTPUT_DIR/$base_name/fastqc"
 		if [[ -n "$base_name" && ! -d "$FASTQC_DIR" ]]; then
 			echo "Criando a pasta de saída nos dados ${base_name}..."
 			mkdir -vp $FASTQC_DIR
 			echo -e "Executando o fastqc nos dados disponíveis em ${base_name}...\n"
-			echo "Entrada: "${INPUT_DIR}/${base_name}" 
+			echo "Entrada: ${INPUT_DIR}/${base_name}" 
 			ls ${INPUT_DIR}/${base_name}
-			#fastqc --noextract --nogroup -o "${FASTQC_DIR}" "${INPUT_DIR}/${base_name}/*"
+			fastqc --noextract --nogroup -o "${FASTQC_DIR}" "${INPUT_DIR}/${base_name}/*"
 		else
 			echo "Dados analisados previamente..."
 		fi
