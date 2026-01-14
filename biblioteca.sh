43c66adfd10281df350597006415a36647929d28
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2026-01-14 16:32:47 -0300

    Update biblioteca.sh

--- biblioteca.sh
+++ biblioteca.sh
@@ -434,12 +434,12 @@ function fastqc () {
 		source "${HOME}/repos/ngs-scripts/param/fastqc.param"
 	
 	# Análise propriamente dita
-	for base_name in $(find "$INPUT_DIR/." -maxdepth 1 -mindepth 1 -type d -exec basename {} \; | sort); do
-		FASTQC_DIR="$OUTPUT_DIR/$base_name/fastqc"
-		echo "Criando a pasta de saída nos dados $base_name..."
-		mkdir -vp $FASTQC_DIR
-		echo -e "Executando o fastqc nos dados disponíveis em $base_name...\n"
-		fastqc --noextract --nogroup -o $FASTQC_DIR "$INPUT_DIR/$base_name/*"
+	for base_name in $(find ${INPUT_DIR}/. -maxdepth 1 -mindepth 1 -type d -exec basename {} \; | sort); do
+		FASTQC_DIR="${OUTPUT_DIR}/${base_name}/fastqc"
+		echo "Criando a pasta de saída nos dados ${base_name}..."
+		mkdir -vp ${FASTQC_DIR}
+		echo -e "Executando o fastqc nos dados disponíveis em ${base_name}...\n"
+		fastqc --noextract --nogroup -o ${FASTQC_DIR} "${INPUT_DIR}/${base_name}/*"
 		#if [[ -n "$base_name" && ! -d "$FASTQC_DIR" ]]; then
 		
 		#else
